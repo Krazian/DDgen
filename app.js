efeca96319d85d38015fd23480128d5b61589f54
Johnathan Chei <j.chei175@gmail.com>
2016-10-07 12:47:20 -0400

    calculating modifiers correctly. need to apply proficiency bonuses

--- app.js
+++ app.js
@@ -18,28 +18,97 @@ app.get("/",function(req,res){
 });
 
 app.get("/basic",function(req,res){
-	// GETS STATS FROM EXPORT MODULE
-	var stats = [];
-	for (var i = 0; i < 7; i++){
-		var number = functions.getStats();
-		stats.push({score: number,modifier:functions.modifiers(number)});
-		}
-		stats.sort(function (a, b) {
-			if (a.score > b.score) {
-				return 1;
-			}
-			if (a.score < b.score) {
-				return -1;
-			}
-			return 0;
-		}).shift();
-
-	// RANDOMLY ASSIGN STATS
-	var abilityScores = new attr(functions.shuffle(stats));
+	function pickRandomAbility(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+	}
 
 	//RANDOMLY SELECT A RACE AND CLASS
 	db.all('SELECT * FROM races ORDER BY RANDOM() LIMIT 1',function(err,race){
 		db.all('SELECT * FROM classes ORDER BY RANDOM() LIMIT 1',function(err,job){
+			// GETS STATS FROM EXPORT MODULE
+			var stats = [];
+			for (var i = 0; i < 7; i++){
+				var number = functions.getStats();
+				stats.push({score: number,modifier:functions.modifiers(number)});
+				}
+				stats.sort(function (a, b) {
+					if (a.score > b.score) {
+						return 1;
+					}
+					if (a.score < b.score) {
+						return -1;
+					}
+					return 0;
+				}).shift();
+
+			// RANDOMLY ASSIGN STATS
+			var abilityScores = new attr(functions.shuffle(stats));
+			race[0].ability_score_bonus.split(",").forEach(function(bonus){
+				var number = bonus.trim().split(" ")[0];
+				var stat = bonus.trim().split(" ")[1];
+				//ADDS RACIAL STAT BONUSES
+				switch (stat){
+					case "Strength":
+						abilityScores.Strength.score += parseInt(number, 10);
+						abilityScores.Strength.modifier = functions.modifiers(abilityScores.Strength.score);
+						break;
+					case "Dexterity":
+						abilityScores.Dexterity.score += parseInt(number, 10);
+						abilityScores.Dexterity.modifier = functions.modifiers(abilityScores.Dexterity.score);
+						break;
+					case "Constitution":
+						abilityScores.Constitution.score += parseInt(number, 10);
+						abilityScores.Constitution.modifier = functions.modifiers(abilityScores.Constitution.score);
+						break;
+					case "Intelligence":
+						abilityScores.Intelligence.score += parseInt(number, 10);
+						abilityScores.Intelligence.modifier = functions.modifiers(abilityScores.Intelligence.score);
+						break;
+					case "Wisdom":
+						abilityScores.Wisdom.score += parseInt(number, 10);
+						abilityScores.Wisdom.modifier = functions.modifiers(abilityScores.Wisdom.score);
+						break;
+					case "Charisma":
+						abilityScores.Charisma.score += parseInt(number, 10);
+						abilityScores.Charisma.modifier = functions.modifiers(abilityScores.Charisma.score);
+						break;
+					default:
+						//SPECIFICALLY FOR HALF ELF WHICH ALLOWS +1 TO ANY 2 ABILITIES
+						for (var times = 0; times < 2; times++){
+							switch (pickRandomAbility(abilityScores)){
+								case "Strength":
+									abilityScores.Strength.score += 1;
+									abilityScores.Strength.modifier = functions.modifiers(abilityScores.Strength.score);
+									break;
+								case "Dexterity":
+									abilityScores.Dexterity.score += 1;
+									abilityScores.Dexterity.modifier = functions.modifiers(abilityScores.Dexterity.score);
+									break;
+								case "Constitution":
+									abilityScores.Constitution.score += 1;
+									abilityScores.Constitution.modifier = functions.modifiers(abilityScores.Constitution.score);
+									break;
+								case "Intelligence":
+									abilityScores.Intelligence.score += 1;
+									abilityScores.Intelligence.modifier = functions.modifiers(abilityScores.Intelligence.score);
+									break;
+								case "Wisdom":
+									abilityScores.Wisdom.score += 1;
+									abilityScores.Wisdom.modifier = functions.modifiers(abilityScores.Wisdom.score);
+									break;
+								case "Charisma":
+									abilityScores.Charisma.score += 1;
+									abilityScores.Charisma.modifier = functions.modifiers(abilityScores.Charisma.score);
+									break;
+							}
+						}
+					}
+			});
 			race[0].male_name = functions.pickSomething(1,race[0].male_name,",");
 			race[0].female_name = functions.pickSomething(1,race[0].female_name,",");
 			race[0].clan_or_family_name = functions.pickSomething(1,race[0].clan_or_family_name,",");
@@ -52,6 +121,7 @@ app.get("/basic",function(req,res){
 			});
 			job[0].starting_equipment = chosenGear;
 			job[0].skills = functions.pickSomething(job[0].choose_skills,job[0].skills,",");
+			console.log(job[0])
 			res.render("basic.ejs",{race:race[0],job:job[0],stat:abilityScores});
 		});
 	});
